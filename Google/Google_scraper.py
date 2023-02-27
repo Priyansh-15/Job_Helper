b8ec5755406dc67e1faa46c8c606b44a94481e92
koustubh1317 <koustubh13sep@gmail.com>
2023-02-27 21:49:58 +0530

    Google - 0.6

--- Google/Google_scraper.py
+++ Google/Google_scraper.py
@@ -15,7 +15,8 @@ options = webdriver.ChromeOptions()
 #setting headless parameter
 options.headless = True
 
-GOOGLE_INTERNSHIP_URL = "https://careers.google.com/jobs/results/?distance=50&employment_type=INTERN"
+GOOGLE_INTERNSHIP_URL = "https://careers.google.com/jobs/results/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=INFORMATION_TECHNOLOGY&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&category=USER_EXPERIENCE&employment_type=INTERN&jex=ENTRY_LEVEL"
+GOOGLE_NEWGRAD_URL="https://careers.google.com/jobs/results/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=INFORMATION_TECHNOLOGY&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&category=USER_EXPERIENCE&employment_type=FULL_TIME&employment_type=PART_TIME&employment_type=TEMPORARY&jex=ENTRY_LEVEL"
 GOOGLE_EXPERIENCED_URL= "https://careers.google.com/jobs/results/?distance=50&employment_type=FULL_TIME"
 fieldnames = ['Company_Name','Job_title','Url','Location','Description']
 
@@ -96,6 +97,63 @@ def Google_job_internship_scrape(index):
 
 
 
+def Google_job_newgrad_scrape(index):
+    google_newgrad=[]
+    location=""
+    description=""
+    driver = webdriver.Chrome(options=options)
+   
+    driver.get(GOOGLE_NEWGRAD_URL)
+    wait=WebDriverWait(driver,10)
+    while True:
+        try:
+            google_job_expand='//*[@id="search-results"]/li['+str(index)+']/div/a/div'
+            wait.until(EC.element_to_be_clickable((By.XPATH,google_job_expand))).click()
+            wait.until(EC.presence_of_element_located((By.XPATH,google_job_title))) #dummy to wait till page is loaded
+
+            page = driver.execute_script('return document.body.innerHTML')
+            soup=BeautifulSoup(''.join(page), 'lxml')
+            dom = etree.HTML(str(soup))
+
+            index+=1 
+
+            #scraped information
+            job_title=dom.xpath(google_job_title)[0].text
+            job_url=driver.current_url
+            location_data=dom.xpath(google_job_location)
+            description_data=dom.xpath(google_job_description)
+
+            for l in location_data:
+                location=etree.tostring(l, pretty_print=True).decode()
+                location=text_decode(location)
+
+            for d in description_data:
+                description=etree.tostring(d, pretty_print=True).decode()
+            
+            job_data={
+                'Company_Name':'Google',
+                'Job_title':job_title,
+                'Url':job_url,
+                'Location':location,
+                'Description':text_decode(description)
+            } 
+            google_newgrad.append(job_data)
+            
+            wait.until(EC.element_to_be_clickable((By.XPATH,google_back_button))).click() 
+            time.sleep(1.5)
+            #wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-results"]/li['+str(index-1)+']/div/a/div')))
+            driver.execute_script("window.scrollTo(0, 1000)") 
+            print("index")
+
+        except TimeoutException:
+            driver.quit()
+            break
+
+    add_to_csv(fieldnames,google_newgrad,'Google_newgrad_openings.csv')
+
+
+
+
 def Google_job_experienced_scrape(index):
     google_experienced=[]
     location=""
@@ -159,6 +217,7 @@ def add_to_csv(fieldnames,google,title):
 
 
 #Google_job_internship_scrape(1)
-Google_job_experienced_scrape(1)
+#Google_job_experienced_scrape(1)
+Google_job_newgrad_scrape(1)
 
 
