754dfad92016709866fe2c0096eb3738a96ffdc9
koustubh1317 <koustubh13sep@gmail.com>
2023-02-27 21:29:36 +0530

    Google - 0.5

--- Google/Google_scraper.py
+++ Google/Google_scraper.py
@@ -16,7 +16,9 @@ options = webdriver.ChromeOptions()
 options.headless = True
 
 GOOGLE_INTERNSHIP_URL = "https://careers.google.com/jobs/results/?distance=50&employment_type=INTERN"
+GOOGLE_EXPERIENCED_URL= "https://careers.google.com/jobs/results/?distance=50&employment_type=FULL_TIME"
 fieldnames = ['Company_Name','Job_title','Url','Location','Description']
+
 
 #LIST OF REQUIRED XPATHS
 google_back_button='/html/body/div[1]/header/div[1]/div[2]/div/div/button'
@@ -92,11 +94,71 @@ def Google_job_internship_scrape(index):
 
     add_to_csv(fieldnames,google_internship,'Google_internship_openings.csv')
 
+
+
+def Google_job_experienced_scrape(index):
+    google_experienced=[]
+    location=""
+    description=""
+    driver = webdriver.Chrome(options=options)
+   
+    driver.get(GOOGLE_EXPERIENCED_URL)
+    wait=WebDriverWait(driver,10)
+    while True:
+        try:
+            google_job_expand='//*[@id="search-results"]/li['+str(index)+']/div/a/div'
+            wait.until(EC.element_to_be_clickable((By.XPATH,google_job_expand))).click()
+            wait.until(EC.presence_of_element_located((By.XPATH,google_job_title))) #dummy to wait till page is loaded
+
+            page = driver.execute_script('return document.body.innerHTML')
+            soup=BeautifulSoup(''.join(page), 'lxml')
+            dom = etree.HTML(str(soup))
+
+            index+=1 
+
+            #scraped information
+            job_title=dom.xpath(google_job_title)[0].text
+            job_url=driver.current_url
+            location_data=dom.xpath(google_job_location)
+            description_data=dom.xpath(google_job_description)
+
+            for l in location_data:
+                location=etree.tostring(l, pretty_print=True).decode()
+                location=text_decode(location)
+
+            for d in description_data:
+                description=etree.tostring(d, pretty_print=True).decode()
+            
+            job_data={
+                'Company_Name':'Google',
+                'Job_title':job_title,
+                'Url':job_url,
+                'Location':location,
+                'Description':text_decode(description)
+            } 
+            google_experienced.append(job_data)
+            
+            wait.until(EC.element_to_be_clickable((By.XPATH,google_back_button))).click() 
+            time.sleep(1.5)
+            wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-results"]/li['+str(index-1)+']/div/a/div')))
+            driver.execute_script("window.scrollTo(0, 1000)") 
+            print("index")
+
+        except TimeoutException:
+            driver.quit()
+            break
+
+    add_to_csv(fieldnames,google_experienced,'Google_experienced_openings.csv')
+
+
+
 def add_to_csv(fieldnames,google,title):
     with open(title, 'w', encoding='UTF8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writerows(google)
 
 
-Google_job_internship_scrape(1)
+#Google_job_internship_scrape(1)
+Google_job_experienced_scrape(1)
+
 
