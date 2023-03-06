5e81db19fdb88912032837ab3a63a78caae1eaa5
Priyansh Sharma <priyansh_sharma_15@outlook.com>
2023-03-06 23:17:51 +0530

    microsoft 0.2

--- Microsoft/Microsoft_scraper.py
+++ Microsoft/Microsoft_scraper.py
@@ -9,3 +9,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+#object of ChromeOptions
+options = webdriver.ChromeOptions()
+#setting headless parameter
+options.headless = True
+
+MICROSOFT_INTERNSHIP_URL = "https://jobs.cisco.com/jobs/SearchJobs/?21180=%5B165%5D&21180_format=6022&21181=%5B186%2C194%2C187%2C55816092%5D&21181_format=6023&listFilterMode=1"
+MICROSOFT_NEWGRAD_URL ="https://jobs.cisco.com/jobs/SearchJobs/?21180=%5B164%5D&21180_format=6022&21181=%5B186%2C194%2C187%2C55816092%5D&21181_format=6023&listFilterMode=1"
+MICROSOFT_EXPERIENCED_URL ="https://jobs.cisco.com/jobs/SearchJobs/?21180=%5B163%5D&21180_format=6022&21181=%5B186%2C194%2C187%2C55816092%5D&21181_format=6023&listFilterMode=1"
+fieldnames = ['Company_Name','Job_title','Url','Location','Description']
+
