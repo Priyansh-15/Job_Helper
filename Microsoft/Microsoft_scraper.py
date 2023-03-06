db0cec5a9affb305571614e901b6585a5e9358e5
Priyansh Sharma <priyansh_sharma_15@outlook.com>
2023-03-06 23:18:23 +0530

    microsoft 0.3

--- Microsoft/Microsoft_scraper.py
+++ Microsoft/Microsoft_scraper.py
@@ -20,3 +20,83 @@ MICROSOFT_NEWGRAD_URL ="https://jobs.cisco.com/jobs/SearchJobs/?21180=%5B164%5D&
 MICROSOFT_EXPERIENCED_URL ="https://jobs.cisco.com/jobs/SearchJobs/?21180=%5B163%5D&21180_format=6022&21181=%5B186%2C194%2C187%2C55816092%5D&21181_format=6023&listFilterMode=1"
 fieldnames = ['Company_Name','Job_title','Url','Location','Description']
 
+
+#LIST OF REQUIRED XPATHS
+microsoft_back_button='//*[@id="content"]/div/div[1]/div/nav/ul/li/a'
+microsoft_job_location='//*[@id="content"]/div/div[2]/div/div[2]/div[2]/div/div/ul/li[1]/div[2]'
+microsoft_job_title='//*[@id="content"]/div/div[2]/div/div[1]/h2'
+microsoft_job_description='//*[@id="content"]/div/div[2]/div/div[2]/div[3]/div/div[1]'
+
+def text_decode(txt):
+    str=""
+    for s in txt:
+        if(s=='>'):
+            t=1
+        elif(s=='<'):
+            t=0
+        if(t==1 and s!='>' and s!='\n'):
+            str+=s
+    return str
+
+def auto():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(MICROSOFT_INTERNSHIP_URL)
+    driver.execute_script("window.scrollTo(0, 300)")  
+    time.sleep(20)  
+
+def Microsoft_job_internship_scrape(index):
+    microsoft_internship=[]
+    location=""
+    description=""
+    driver = webdriver.Chrome(options=options)
+    driver.get(MICROSOFT_INTERNSHIP_URL)
+    driver.execute_script("window.scrollTo(0, 300)")
+    wait=WebDriverWait(driver,10)
+    wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="onetrust-close-btn-container"]/button'))).click()
+    
+    while True:
+        try:
+            microsoft_job_expand='//*[@id="content"]/div/div[2]/table/tbody/tr['+str(index)+']/td[1]/a'
+            wait.until(EC.element_to_be_clickable((By.XPATH,microsoft_job_expand))).click()
+            wait.until(EC.presence_of_element_located((By.XPATH,microsoft_job_title))) #dummy to wait till page is loaded
+
+            page = driver.execute_script('return document.body.innerHTML')
+            soup=BeautifulSoup(''.join(page), 'lxml')
+            dom = etree.HTML(str(soup))
+
+            index+=1 
+
+            #scraped information
+            job_title=dom.xpath(microsoft_job_title)[0].text
+            job_url=driver.current_url
+            location_data=dom.xpath(microsoft_job_location)
+            description_data=dom.xpath(microsoft_job_description)
+
+            for l in location_data:
+                location=etree.tostring(l, pretty_print=True).decode()
+                location=text_decode(location)
+
+            for d in description_data:
+                description+=etree.tostring(d, pretty_print=True).decode()
+
+            job_data={
+                'Company_Name':'Microsoft',
+                'Job_title':job_title,
+                'Url':job_url,
+                'Location':location,
+                'Description':text_decode(description)
+            } 
+            microsoft_internship.append(job_data)
+            
+            wait.until(EC.element_to_be_clickable((By.XPATH,microsoft_back_button))).click() 
+            wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div[2]/table/tbody/tr['+str(index)+']/td[1]/a')))
+            driver.execute_script("window.scrollTo(0, 800)")
+            print("index")
+
+        except TimeoutException:
+            driver.quit()
+            break
+
+    add_to_csv(fieldnames,microsoft_internship,'Microsoft_internship_openings.csv')
+
