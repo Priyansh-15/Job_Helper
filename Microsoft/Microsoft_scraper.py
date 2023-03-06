3f7f5cf21a661d9f66a928684469e0eaf20872c5
Priyansh Sharma <priyansh_sharma_15@outlook.com>
2023-03-06 23:17:25 +0530

    microsoft 0.1

---
+++ Microsoft/Microsoft_scraper.py
@@ -0,0 +1,11 @@
+import time
+import csv
+from bs4 import BeautifulSoup
+from IPython.display import HTML
+from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
