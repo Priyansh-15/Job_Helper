75d2fc5f3be8845f5b0dd552dfda34a024d7d781
koustubh1317 <koustubh13sep@gmail.com>
2023-03-11 10:59:54 +0530

    ui - 0.5

--- ui/src/pages/Homepage.js
+++ ui/src/pages/Homepage.js
@@ -11,10 +11,62 @@ const Homepage = () => {
             <div className='user_form_title'>
               User Profile Details
             </div>
+            <form>
             <div className='form'>
-                <div className='section_head'>Personal details</div>
+              
+                <div className='section_1'>
+                  <fieldset>
+                    <legend>Personal Details</legend>
+                    <div className='main_section_1'>
+                        <div className='labels'>
+                          <span>Name</span>
+                          <br/>
+                            <input type="text"  className='search_box' name="name"/>
+                        </div>
+                            
+                      <div className='labels'>
+                         <span>Email</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="email"/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span>Phone Number</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="phoneno"/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span>College</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="college"/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span> Degree</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="degree"/>
+                      </div>
+                     
+                      <div className='labels'>
+                      <span>Major</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="major"/>
+                      </div>
+                      <div className='labels'>
+                          <span>GPA</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="gpa"/>
+                      </div>
+                      
+                      </div>
+                  </fieldset>
+                </div>
+             
+                
                 
             </div>
+            </form>
          </div>
     </div>
   )
