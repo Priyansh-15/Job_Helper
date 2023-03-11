216e862d942feb881f8c151706eaf242dae6daca
koustubh1317 <koustubh13sep@gmail.com>
2023-03-11 12:31:00 +0530

    ui - 0.6

--- ui/src/pages/Homepage.js
+++ ui/src/pages/Homepage.js
@@ -21,19 +21,19 @@ const Homepage = () => {
                         <div className='labels'>
                           <span>Name</span>
                           <br/>
-                            <input type="text"  className='search_box' name="name"/>
+                            <input type="text"  className='search_box' name="name" placeholder='Full Name'/>
                         </div>
                             
                       <div className='labels'>
                          <span>Email</span>
                           <br/>
-                            <input type="text" className='search_box'  name="email"/>
+                            <input type="text" className='search_box'  name="email" placeholder='xyz@mail.com'/>
                       </div>
                       
                       <div className='labels'>
                       <span>Phone Number</span>
                           <br/>
-                            <input type="text" className='search_box'  name="phoneno"/>
+                            <input type="text" className='search_box'  name="phoneno" placeholder='xxxxxxxxxx'/>
                       </div>
                       
                       <div className='labels'>
@@ -51,21 +51,136 @@ const Homepage = () => {
                       <div className='labels'>
                       <span>Major</span>
                           <br/>
-                            <input type="text" className='search_box'  name="major"/>
+                            <input type="text" className='search_box'  name="major" />
                       </div>
                       <div className='labels'>
                           <span>GPA</span>
                           <br/>
-                            <input type="text" className='search_box'  name="gpa"/>
+                            <input type="text" className='search_box'  name="gpa" placeholder='scale of 10'/>
                       </div>
                       
                       </div>
                   </fieldset>
                 </div>
-             
                 
+               
                 
+                <div className='section_2'>
+                  <fieldset>
+                    <legend>Skill Evaluation</legend>
+                    <div className='main_section_2'>
+                      <div className='labels'>
+                         <span>C++/C</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="c++/c" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span>Python</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="python" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span>Java</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="java" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      
+                      <div className='labels'>
+                      <span>JavaScript</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="javascript" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>Data Structures and Algorithms</span>
+                          <br/>
+                            <input type="text"  className='search_box' name="dsa" placeholder='Rate on scale(1-10)'/>
+                        </div>
+                      <div className='labels'>
+                      <span>Problem Solving</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="problem" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>MySQL</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="sql" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>Web services</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="web" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>HTML/CSS</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="html/css" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      </div>
+                  </fieldset>
+                </div>
+                <div className='section_3'>
+                  <fieldset>
+                    <legend>Soft Skills</legend>
+                    <div className='main_section_3'>
+                    <div className='labels'>
+                          <span>Reading, Writing, Speaking (English)</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="english" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>Team work</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="team work" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>Leadership</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="leadership" placeholder='Rate on scale(1-10)'/>
+                      </div>
+                      <div className='labels'>
+                          <span>Adaptability</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="adapt" placeholder='Rate on scale(1-10)'/>
+                      </div> 
+                      </div>
+                  </fieldset>
+                </div>
+                <div className='section_4'>
+                  <fieldset>
+                    <legend>Websites and other Links</legend>
+                    <div className='main_section_4'>
+                    <div className='labels'>
+                          <span>Portfolio</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="portfolio"/>
+                      </div> 
+                      <div className='labels'>
+                          <span>Github</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="git"/>
+                      </div>
+                      <div className='labels'>
+                          <span>LinkedIn</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="linked"/>
+                      </div> 
+                      <div className='labels'>
+                          <span>LeetCode</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="leetcode"/>
+                      </div>  
+                      <div className='labels'>
+                          <span>Codeforces</span>
+                          <br/>
+                            <input type="text" className='search_box'  name="cf"/>
+                      </div> 
+                      </div>
+                  </fieldset>
+                </div>
             </div>
+          
             </form>
          </div>
     </div>
