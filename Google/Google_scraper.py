aa03cb318fde730072ab351ac52a66f45caeb068
koustubh1317 <koustubh13sep@gmail.com>
2023-03-17 11:47:36 +0530

    scraping changes

--- Google/Google_scraper.py
+++ Google/Google_scraper.py
@@ -142,7 +142,7 @@ def Google_job_newgrad_scrape(index):
             wait.until(EC.element_to_be_clickable((By.XPATH,google_back_button))).click() 
             time.sleep(1.5)
             #wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-results"]/li['+str(index-1)+']/div/a/div')))
-            driver.execute_script("window.scrollTo(0, 1000)") 
+            driver.execute_script("window.scrollTo(0, 1080)") 
             print("index")
 
         except TimeoutException:
@@ -198,7 +198,7 @@ def Google_job_experienced_scrape(index):
             wait.until(EC.element_to_be_clickable((By.XPATH,google_back_button))).click() 
             time.sleep(1.5)
             wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-results"]/li['+str(index-1)+']/div/a/div')))
-            driver.execute_script("window.scrollTo(0, 1000)") 
+            driver.execute_script("window.scrollTo(0, 1080)") 
             print("index")
 
         except TimeoutException:
--- ui/src/pages/Loading.js
+++ ui/src/pages/Loading.js
@@ -29,7 +29,7 @@ const Loading = () => {
             </div>
          </div>
          <div class="text">
-            Extracting matched jobs...
+            Just a moment...
          </div>
       </div>
   )
